ebea9d92a58435675f37c3860bbf826ce4d25666
Márcio DEV <playdznvlt@gmail.com>
2026-01-14 13:46:08 -0300

    feat: implement FastifyModule with server initialization and options handling

--- src/Module.ts
+++ src/Module.ts
@@ -1,16 +1,44 @@
 import { Core, BaseModule, Module } from '@zyrohub/core';
+import fastify, {
+	FastifyBaseLogger,
+	FastifyHttpOptions,
+	FastifyInstance,
+	FastifyListenOptions,
+	RawServerDefault
+} from 'fastify';
 
-export interface FastifyModuleOptions {}
+export interface FastifyModuleOptions {
+	port?: number | string;
+	host?: string;
+
+	rawOptions?: FastifyHttpOptions<RawServerDefault, FastifyBaseLogger> | undefined;
+	rawListenOptions?: FastifyListenOptions;
+}
 
 @Module()
 export class FastifyModule extends BaseModule {
 	static options: FastifyModuleOptions;
 
+	server?: FastifyInstance;
+
 	constructor() {
 		super();
 	}
 
-	async init(data: { core: Core; options: FastifyModuleOptions }) {}
+	async init(data: { core: Core; options: FastifyModuleOptions }) {
+		this.server = fastify({
+			...data.options.rawOptions
+		});
+
+		const parsedPort = typeof data.options.port === 'string' ? parseInt(data.options.port, 10) : data.options.port;
+
+		this.server.listen({
+			port: parsedPort || 3000,
+			host: data.options.host,
+
+			...data.options.rawListenOptions
+		});
+	}
 }
 
 export default { FastifyModule };
